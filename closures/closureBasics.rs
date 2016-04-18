6afe31c46fa230398b1accf2d031a3c3e2012148
Kalin <kalin@Ubuntu-7010.(none)>
2016-04-17 20:52:33 -0500

    update

--- closures/closureBasics.rs
+++ closures/closureBasics.rs
@@ -1,22 +1,22 @@
-/* closure basics:
-
-   A closure is an unnamed function that can pick up data from the
+/* 
+   A Rust closure is an unnamed function that can pick up data from the
    enclosing environment. A closure is invoked with the same syntax as
    a named function, but can be defined with less syntax. This short
    program illustrates the basics. 
 
-   Closures are used in Rust multi-threading.
+   Closures are important for Rust multi-threading.
  */
 
 fn main() {
-    let base_value = 100;
+    let base_value = 100; // binding whose value is a data item
 
     // The binding of 'inc1' to an unnamed function, which takes two arguments
     // named 'x' and 'y' in this case. Rust can infer the types for the arguments,
     // although the types could be given explicitly (in this case, i32 is the type
     // for each argument and the return value).
     // The name 'inc1' is bound to the parameterized code on the right of the =.
-    let inc1 = |x, y| {   // syntax similar to that of a Ruby block
+    // (The Rust syntax for closures is close to the Ruby syntax for unnamed blocks.)
+    let inc1 = |x, y| {   // binding whose value is an unnamed function
         x + y 
     };
 
@@ -29,5 +29,38 @@ fn main() {
     for i in 0..10 {
         println!("{} {}", inc1(i, i + 1), inc2(i, i + 1)); // 1 3 5..19, 101 103 105...119
     }    
+
+    // A closure with no arguments.
+    let mut counter_init_value = 0;
+    let mut counter = || {   // mutable because mutable count_init_value is borrowed in the calls below
+        counter_init_value += 1;
+        counter_init_value
+    };
+    
+    for _ in 0..10 {
+        println!("{}", counter()); // 1 2 3...9 10
+    }
+    //#########################
+
+    // 'moving' the environment to the closure itself
+    let mut n1 = 123; 
+    {
+        let mut add_to_n1 = |x| { 
+            n1 += x;               // n1 above is changed
+        };
+        add_to_n1(-123); 
+    }
+    assert_eq!(n1, 0);             // n1 is now 0
+
+    let mut n2 = 987;
+    {
+        let mut add_to_n2 = move |x| {  // the 'move' gives the closure its own (stack) copy of n2
+            n2 += x;                    // only local copy of n2 is changed
+        };
+        add_to_n2(-987);
+    }
+    assert_eq!(n2, 987);                // n2 is still 987
 }
+
+
 
---
+++ closures/thread2.rs
@@ -0,0 +1,17 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    let mut n = 0;
+
+    //### Try to create a race condition by spawning 32 threads, each of which
+    //    then tries to increment n.
+    for _ in 0..32 {
+        //### Remove the 'move', and the program won't compile.
+        thread::spawn(move || {  // because of 'move', no race condition is possible
+            n += 1;
+        });
+    }
+    thread::sleep(Duration::from_millis(3000));
+    println!("{}", n);
+}
